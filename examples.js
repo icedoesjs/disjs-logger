e94b3be5b3ebb46775e270e4f6c4b22d27d2fcd3
iceyym8 <40555230+iceyym8@users.noreply.github.com>
2021-08-03 00:52:31 -0400

    Update examples.js

--- examples.js
+++ examples.js
@@ -1,4 +1,4 @@
 const logger = require('disjs-logger');
-const console = new logger('mylogfile.txt');
+const console = new logger('C:\\Users\\Username\\ProjectDirectory\\logfile.txt');
 // Error Logging 
 console.log("My error here", "error", __filename, `There was an error, please check the log file`)
